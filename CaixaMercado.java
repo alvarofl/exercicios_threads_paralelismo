9a51e60326e62cfa52bb8836288d91ef55c3955a
alvarofl <alvarolima837@gmail.com>
2025-09-24 11:33:02 -0300

    corrigindo os bugs, erro na declaração dentro do for, e corrigindo as chaves fechado no lugar errado

--- CaixaMercado.java
+++ CaixaMercado.java
@@ -11,26 +11,27 @@ public class CaixaMercado {
         public static void main(String[] args) {
             //instancia o objeto
 
-                CaixaMercado Mercado = new CaixaMercado();
-                for (String caixa : mercado.caixas) {
-                        new Thread = (() -> {
-                                            System.out.println(caixa + " iniciou atendimento na " + 
-                                                Thread.currentThread().getName());
+                CaixaMercado mercado = new CaixaMercado();
+                for (String caixa : mercado.caixa) {
+                        new Thread(() -> {
+                                            System.out.println(caixa + " iniciou atendimento na " +Thread.currentThread().getName());
+
+    
+
                             try {
-                                //simulando cada parte do processo de compra, passaCompra, pagandoCompra e pagaCompra
+                                
                                         for(Processo p : Processo.values()) {
                                                                     System.out.println(caixa + " está em: " + p);
                                             Thread.sleep(1000   );
                                             //esse comando thread sleep faz uma pausa
-
+                                        }
                                         }   catch (InterruptedException e) {
                                                                 e.printStackTrace();
 
                                         }
                 System.out.println(caixa + " finalizou atendimento ✅");
                             }).start();
-                        }
+                                       }                       }
 
-                }
+                
         }
-}
